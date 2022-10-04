a53642431cbcc921515c292d7e3d236244c7978b
法外狂徒李四 <dimensional_logic@qq.com>
2022-10-04 17:29:47 +0800

    【i】【service-engine-web-v1】

--- auth/src/main/java/com/cmcorg/engine/web/auth/model/entity/SysUserInfoDO.java
+++ auth/src/main/java/com/cmcorg/engine/web/auth/model/entity/SysUserInfoDO.java
@@ -1,6 +1,5 @@
 package com.cmcorg.engine.web.auth.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,7 +10,7 @@ import lombok.Data;
 @Schema(description = "用户基本信息表")
 public class SysUserInfoDO {
 
-    @TableId(type = IdType.ASSIGN_ID)
+    @TableId
     @Schema(description = "用户主键 id")
     private Long id;
 
