9d0d85299279f3536e8de9bc5f2d550392ca2ea2
法外狂徒李四 <dimensional_logic@qq.com>
2022-10-22 13:37:40 +0800

    【i】【CmcOrg】

--- web-auth/src/main/java/com/cmcorg/engine/web/auth/configuration/mybatisplus/MybatisPlusConfiguration.java
+++ web-auth/src/main/java/com/cmcorg/engine/web/auth/configuration/mybatisplus/MybatisPlusConfiguration.java
@@ -3,9 +3,13 @@ package com.cmcorg.engine.web.auth.configuration.mybatisplus;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import com.cmcorg.engine.web.redisson.util.IdGeneratorUtil;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -20,6 +24,19 @@ public class MybatisPlusConfiguration {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
 
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+
+        // 如果用了分页插件注意先 add TenantLineInnerInterceptor 再 add PaginationInnerInterceptor
+        mybatisPlusInterceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
+            @Override
+            public Expression getTenantId() {
+                return new LongValue(1);
+            }
+
+            @Override
+            public boolean ignoreTable(String tableName) {
+                return true;
+            }
+        }));
 
         // 分页插件
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
