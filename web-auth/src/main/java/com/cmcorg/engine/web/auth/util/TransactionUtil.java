cdde8c4e5971975621e6751780420db7a7194fa5
caoguiming <1677664380@qq.com>
2022-12-21 18:30:27 +0800

    【i】【CmcOrg】

---
+++ web-auth/src/main/java/com/cmcorg/engine/web/auth/util/TransactionUtil.java
@@ -0,0 +1,47 @@
+package com.cmcorg.engine.web.auth.util;
+
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+
+import java.util.function.Supplier;
+
+@Component
+public class TransactionUtil {
+
+    private static DataSourceTransactionManager dataSourceTransactionManager;
+    private static TransactionDefinition transactionDefinition;
+
+    public TransactionUtil(DataSourceTransactionManager dataSourceTransactionManager,
+        TransactionDefinition transactionDefinition) {
+        TransactionUtil.dataSourceTransactionManager = dataSourceTransactionManager;
+        TransactionUtil.transactionDefinition = transactionDefinition;
+    }
+
+    /**
+     * 携带事务，执行
+     */
+    public static <T> T transactionExec(Supplier<T> supplier) {
+
+        // 开启事务
+        TransactionStatus transactionStatus = dataSourceTransactionManager.getTransaction(transactionDefinition);
+
+        try {
+
+            T resObject = supplier.get();
+
+            dataSourceTransactionManager.commit(transactionStatus); // 提交
+
+            return resObject;
+
+        } catch (Exception e) {
+
+            dataSourceTransactionManager.rollback(transactionStatus); // 回滚
+            throw e;
+
+        }
+
+    }
+
+}
