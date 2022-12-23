61c0df239b9751d7ffc1a74b2e8ed1defa574fce
caoguiming <1677664380@qq.com>
2022-12-23 17:24:43 +0800

    【i】【CmcOrg】

--- web-auth/src/main/java/com/cmcorg/engine/web/auth/configuration/JacksonConfiguration.java
+++ web-auth/src/main/java/com/cmcorg/engine/web/auth/configuration/JacksonConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class JacksonConfiguration {
 
     /**
-     * Jackson全局转化 Long类型为 String，解决 jackson序列化时传入前端 Long类型缺失精度问题
+     * jackson全局转化 Long类型为 String，解决 jackson序列化时传入前端 Long类型缺失精度问题
      */
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
