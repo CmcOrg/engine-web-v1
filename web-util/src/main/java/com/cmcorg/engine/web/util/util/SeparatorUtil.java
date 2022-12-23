b2fb114733f74f820d615871c098dc1d8a5ba08b
caoguiming <1677664380@qq.com>
2022-12-23 16:29:18 +0800

    【i】【CmcOrg】

---
+++ web-util/src/main/java/com/cmcorg/engine/web/util/util/SeparatorUtil.java
@@ -0,0 +1,27 @@
+package com.cmcorg.engine.web.util.util;
+
+import cn.hutool.core.collection.CollUtil;
+
+/**
+ * 连接符，工具类
+ */
+public class SeparatorUtil {
+
+    // 竖线分隔符
+    public final static String VERTICAL_LINE_SEPARATOR = "|";
+
+    /**
+     * 返回：被【竖线分隔符】，包裹的字符串
+     */
+    public static String verticalLine(String str) {
+        return VERTICAL_LINE_SEPARATOR + str + VERTICAL_LINE_SEPARATOR;
+    }
+
+    /**
+     * @return 示例：|1||2||3|
+     */
+    public static <T> String verticalLine(Iterable<T> iterable) {
+        return CollUtil.join(iterable, "", VERTICAL_LINE_SEPARATOR, VERTICAL_LINE_SEPARATOR);
+    }
+
+}
