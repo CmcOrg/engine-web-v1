eccf657cdd5148f3a05d8889851359aa69cff99b
法外狂徒李四 <dimensional_logic@qq.com>
2023-01-01 11:21:59 +0800

    【i】【CmcOrg】

--- web-util/src/main/java/com/cmcorg/engine/web/util/util/CallBack.java
+++ web-util/src/main/java/com/cmcorg/engine/web/util/util/CallBack.java
@@ -1,8 +1,12 @@
 package com.cmcorg.engine.web.util.util;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CallBack<T> {
 
     private T value;
